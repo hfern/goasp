09528923730f0670ae12fbcc3c14c939321c4397
Hunter <h.g.fern@gmail.com>
2014-12-11 20:18:15 -0800

    Move test file

--- goasp.go
+++ goasp.go
@@ -1,8 +1,17 @@
 package goasp
 
+import (
+	"io"
+)
+
 // Encoder encodes values to ASP.net
 type Encoder struct {
 }
 
 type Decoder struct {
+}
+
+func Decode(source io.Reader) (interface{}, error) {
+	prs := newParser(source)
+	return decode_full(&prs)
 }
--- goasp_test.go
+++ goasp_test.go
@@ -1,23 +1,30 @@
 package goasp
 
 import (
+	"bytes"
 	"encoding/base64"
 	"io/ioutil"
 	"testing"
 )
 
-func Test_ComplexSRJC(t *testing.T) {
+func TestDecode(t *testing.T) {
 	encoded, err := ioutil.ReadFile("./testdata/srjc_schedule.b64.txt")
 	if err != nil {
 		t.Fatal(err)
 		t.FailNow()
 	}
-	t.Log(encoded)
 
 	binary, err := base64.StdEncoding.DecodeString(string(encoded))
 	if err != nil {
 		t.Fatal(err)
 		t.FailNow()
+	}
+
+	_, err = Decode(bytes.NewBuffer(binary))
+
+	if err != nil {
+		t.Log(err.Error())
+		t.Fail()
 	}
 
 }
